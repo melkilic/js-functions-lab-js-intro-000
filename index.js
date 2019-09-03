17c9a55ae2fcf04623c75fefb4563df21dac5601
Melike KILIC <melike180400@gmail.com>
2019-09-03 17:38:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-  function happyHolidaysTo('Mel'){
-  return 'Happy holidays, ${name}!' ;
-
-}
-function happyHolidayTo("July 4 th, Mel"){
-  return 'Happy ${holiday}, ${name}!' ;
-}
-  function holidayCountdown("71,Veterans Day"){
-  return "It's ${days} days until ${holiday}!" ;
-}
+//   function happyHolidaysTo('Mel'){
+//   return 'Happy holidays, ${name}!' ;
+// 
+// }
+// function happyHolidayTo("July 4 th, Mel"){
+//   return 'Happy ${holiday}, ${name}!' ;
+// }
+//   function holidayCountdown("71,Veterans Day"){
+//   return "It's ${days} days until ${holiday}!" ;
+// }
